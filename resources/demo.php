a4f46f90a21f5e44ceb71d496e363258ff97f6fc
jawira <dev@tugal.be>
2018-12-04 22:06:45 +0100

    Improve documentation

---
+++ resources/demo.php
@@ -0,0 +1,19 @@
+<?php
+
+require __DIR__ . '/../vendor/autoload.php';
+
+use Jawira\MiniGetopt\MiniGetopt;
+
+// Setup
+$getopt = new MiniGetopt();
+$getopt->addRequired('f', 'format', 'File format'); // <1>
+$getopt->addOptional('m', 'message', 'Message'); // <2>
+$getopt->addNoValue('h', 'help', 'Show help'); // <3>
+
+// Retrieving values
+$format  = $getopt->getOption('f', 'format', 'png'); // <4>
+$message = $getopt->getOption('r', 'retry'); // <5>se
+$help    = $getopt->getOption('h', 'help');
+
+// Show retrieved values
+var_dump($format, $message, $help);
